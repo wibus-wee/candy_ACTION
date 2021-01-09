2ec3a72a961c4ae52ca3bacb345669eb38886609
wibus-wee <1596355173@qq.com>
2021-01-09 15:25:31 +0800

    update

--- play.sh
+++ play.sh
@@ -742,4 +742,21 @@ endTime=`date +%s`
 echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"
 read -p "强制启动: " yes;
 
+yarn
+yarn build
 
+
+yarn
+yarn build
+yarn
+yarn build
+yarn
+yarn build
+yarn
+yarn build
+yarn
+yarn build
+yarn
+yarn build
+yarn
+yarn build
